cad803607e44ec6db00f952834c4d5d457240d7a
Jovan Ranković <jovan-rankovic@users.noreply.github.com>
2019-06-08 19:51:27 +0200

    DTO tweaks

--- Application/DataTransfer/ArticleDto.cs
+++ Application/DataTransfer/ArticleDto.cs
@@ -9,18 +9,23 @@ namespace Application.DataTransfer
         [MinLength(3, ErrorMessage = "Title must contain at least 3 characters.")]
         [MaxLength(50, ErrorMessage = "Title can have up to 50 characters.")]
         public string Title { get; set; }
+
         [Required]
         [MinLength(3, ErrorMessage = "Content must contain at least 3 characters.")]
         [DataType(DataType.MultilineText)]
         public string Content { get; set; }
+
         [Required]
         [DataType(DataType.ImageUrl)]
+        [FileExtensions(ErrorMessage = "Invalid extension. (allowed: jpg, jpeg, png)", Extensions = "jpg,jpeg,png")]
         public string Image { get; set; }
+
+        [Display(Name = "Posted on")]
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy. HH:mm}")]
         public System.DateTime CreatedAt { get; set; }
 
         public string Author { get; set; }
-        [Display(GroupName = "Categories for the article")]
+        [Display(GroupName = "Categories")]
         public IEnumerable<string> CategoriesForArticle { get; set; }
     }
 }
--- Application/DataTransfer/UserDto.cs
+++ Application/DataTransfer/UserDto.cs
@@ -11,17 +11,21 @@ namespace Application.DataTransfer
         [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Letters only.")]
         [Display(Name = "First name")]
         public string FirstName { get; set; }
+
         [Required]
         [MinLength(3, ErrorMessage = "Last name must contain at least 3 letters.")]
         [MaxLength(30, ErrorMessage = "Last name can have up to 30 letters.")]
         [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Letters only.")]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
+
         [Required]
         [MinLength(3, ErrorMessage = "Email must contain at least 3 characters.")]
         [MaxLength(60, ErrorMessage = "Email can have up to 60 characters.")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Email is not valid.")]
         public string Email { get; set; }
+
         [Required]
         [MinLength(6, ErrorMessage = "Password must contain at least 6 characters.")]
         [MaxLength(32, ErrorMessage = "Password can have up to 32 characters.")]
