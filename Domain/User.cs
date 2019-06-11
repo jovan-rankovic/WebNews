7b5867fd243f025932a91d60354a713a9354b9be
Jovan Ranković <jovan-rankovic@users.noreply.github.com>
2019-06-11 23:47:43 +0200

    added nullable RoleId in Domain

--- Domain/User.cs
+++ Domain/User.cs
@@ -8,7 +8,7 @@ namespace Domain
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public int RoleId { get; set; }
+        public int? RoleId { get; set; }
 
         public Role Role { get; set; }
         public ICollection<Article> Articles { get; set; }
