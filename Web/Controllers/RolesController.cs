149e398cb5822b8d61e29463f6984f062588a66c
Jovan Ranković <jovan-rankovic@users.noreply.github.com>
2019-06-16 01:05:55 +0200

    added CRUD for Users and Roles in WebApp

---
+++ Web/Controllers/RolesController.cs
@@ -0,0 +1,186 @@
+﻿using Application.Commands.Role;
+using Application.DataTransfer;
+using Application.Exceptions;
+using Application.Searches;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Web.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly IGetRolesCommand _searchRolesCommand;
+        private readonly IGetRoleCommand _getOneRoleCommand;
+        private readonly ICreateRoleCommand _createRoleCommand;
+        private readonly IEditRoleCommand _editRoleCommand;
+        private readonly IDeleteRoleCommand _deleteRoleCommand;
+
+        public RolesController(IGetRolesCommand searchRolesCommand, IGetRoleCommand getOneRoleCommand, ICreateRoleCommand createRoleCommand, IEditRoleCommand editRoleCommand, IDeleteRoleCommand deleteRoleCommand)
+        {
+            _searchRolesCommand = searchRolesCommand;
+            _getOneRoleCommand = getOneRoleCommand;
+            _createRoleCommand = createRoleCommand;
+            _editRoleCommand = editRoleCommand;
+            _deleteRoleCommand = deleteRoleCommand;
+        }
+
+        // GET: Roles
+        public ActionResult Index([FromQuery] RoleSearch roleSearch)
+        {
+            roleSearch.PageNumber = 0;
+            var roles = _searchRolesCommand.Execute(roleSearch);
+            return View(roles.Data);
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                return View(_getOneRoleCommand.Execute(id));
+            }
+            catch (EntityNotFoundException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(RoleDto roleDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "An error occured.";
+                return View();
+            }
+
+            try
+            {
+                _createRoleCommand.Execute(roleDto);
+                TempData["success"] = "Role created.";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (EntityAlreadyExistsException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+            }
+
+            return View();
+        }
+
+        // GET: Roles/Edit/5
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                return View(_getOneRoleCommand.Execute(id));
+            }
+            catch (EntityNotFoundException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: Roles/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, RoleDto roleDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "An error occured.";
+                return View();
+            }
+
+            try
+            {
+                _editRoleCommand.Execute((id, roleDto));
+                TempData["success"] = "Role modified.";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (EntityNotFoundException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (EntityAlreadyExistsException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+            }
+
+            return View();
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(short id)
+        {
+            try
+            {
+                return View(_getOneRoleCommand.Execute(id));
+            }
+            catch (EntityNotFoundException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                _deleteRoleCommand.Execute(id);
+                TempData["success"] = "Role deleted.";
+            }
+            catch (EntityNotFoundException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (EntityDeleteForbiddenException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Web/Controllers/UsersController.cs
@@ -0,0 +1,185 @@
+﻿using Application.Commands.User;
+using Application.DataTransfer;
+using Application.Exceptions;
+using Application.Searches;
+using EfDataAccess;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IGetUsersCommand _searchUsersCommand;
+        private readonly IGetUserCommand _getOneUserCommand;
+        private readonly ICreateUserCommand _createUserCommand;
+        private readonly IEditUserCommand _editUserCommand;
+        private readonly IDeleteUserCommand _deleteUserCommand;
+        private readonly WebNewsContext _context;
+
+        public UsersController(IGetUsersCommand searchUsersCommand, IGetUserCommand getOneUserCommand, ICreateUserCommand createUserCommand, IEditUserCommand editUserCommand, IDeleteUserCommand deleteUserCommand, WebNewsContext context)
+        {
+            _searchUsersCommand = searchUsersCommand;
+            _getOneUserCommand = getOneUserCommand;
+            _createUserCommand = createUserCommand;
+            _editUserCommand = editUserCommand;
+            _deleteUserCommand = deleteUserCommand;
+            _context = context;
+        }
+
+        // GET: Users
+        public ActionResult Index([FromQuery] UserSearch userSearch)
+        {
+            userSearch.PageNumber = 0;
+            var users = _searchUsersCommand.Execute(userSearch);
+            return View(users);
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                return View(_getOneUserCommand.Execute(id));
+            }
+            catch (EntityNotFoundException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UserDto userDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "An error occured.";
+                return View();
+            }
+
+            try
+            {
+                _createUserCommand.Execute(userDto);
+                TempData["success"] = "User created.";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (EntityAlreadyExistsException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+            }
+
+            return View();
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                return View(_getOneUserCommand.Execute(id));
+            }
+            catch (EntityNotFoundException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, UserDto userDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "An error occured.";
+                return View();
+            }
+
+            try
+            {
+                _editUserCommand.Execute((id, userDto));
+                TempData["success"] = "User modified.";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (EntityNotFoundException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (EntityAlreadyExistsException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+            }
+
+            return View();
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(short id)
+        {
+            try
+            {
+                return View(_getOneUserCommand.Execute(id));
+            }
+            catch (EntityNotFoundException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                _deleteUserCommand.Execute(id);
+                TempData["success"] = "User deleted.";
+            }
+            catch (EntityNotFoundException e)
+            {
+                TempData["error"] = e.Message;
+            }
+            catch (Exception e)
+            {
+                TempData["error"] = e.Message;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
